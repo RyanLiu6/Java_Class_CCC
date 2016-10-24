1977c3fe07e87072c9c93b1c78eb25b72fc69744
Ryan Liu <Ryan Liu>
2016-10-23 19:54:29 -0700

    Consistent naming scheme

--- Week 2/Main.java
+++ Week 2/Main.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 import static java.lang.Math.*;
 
-public class Main {
+public class Main { 
 
     public static void main(String[] args) {
         // Uses Scanner to read an input
